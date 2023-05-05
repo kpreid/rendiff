7884f3d9b667e6df89960bc25d6a3f5e1fdd02b2
Kevin Reid <kpreid@switchb.org>
2023-05-05 16:43:57 -0700

    Add CLI package.
    This will serve as an interactive example or allow usage from tests
    not written in Rust.

---
+++ cli/src/main.rs
@@ -0,0 +1,67 @@
+use std::path::{Path, PathBuf};
+use std::process::ExitCode;
+
+use anyhow::Context;
+use clap::Parser;
+
+use image::RgbaImage;
+use rendiff::Threshold;
+
+#[derive(Debug, Parser)]
+struct Args {
+    actual: PathBuf,
+    expected: PathBuf,
+
+    #[arg(long = "diff-image", short = 'o')]
+    diff: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<ExitCode> {
+    let Args {
+        actual,
+        expected,
+        diff: diff_path,
+    } = Args::parse();
+
+    let actual = open_with_context("actual image", &actual)?;
+    let expected = open_with_context("expected image", &expected)?;
+
+    let difference = rendiff::diff(&actual, &expected);
+
+    if let (Some(diff_image), Some(diff_path)) = (difference.diff_image, diff_path) {
+        diff_image
+            .save(&diff_path)
+            .with_context(|| format!("failed to write '{}'", diff_path.display()))?;
+    }
+
+    // TODO: args for threshold
+    let equal = Threshold::no_bigger_than(0).allows(difference.histogram);
+
+    // TODO: print the histogram
+
+    Ok(if equal {
+        ExitCode::SUCCESS
+    } else {
+        ExitCode::FAILURE
+    })
+}
+
+fn open_with_context(description: &str, path: &Path) -> anyhow::Result<RgbaImage> {
+    let image = image::open(path)
+        .with_context(|| format!("failed to open {} '{}'", description, path.display()))?;
+    match image {
+        image::DynamicImage::ImageRgba8(rgba_image) => Ok(rgba_image),
+
+        image::DynamicImage::ImageLuma8(_)
+        | image::DynamicImage::ImageLumaA8(_)
+        | image::DynamicImage::ImageRgb8(_) => {
+            // Non-identity but non-lossy conversion
+            Ok(image.to_rgba8())
+        }
+
+        other => {
+            eprintln!("warning: converting image to RGBA8, which is lossy");
+            Ok(other.to_rgba8())
+        }
+    }
+}
