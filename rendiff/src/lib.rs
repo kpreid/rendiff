60dade3a9e1db0af1d002458f3da8feb3f0a2d44
Kevin Reid <kpreid@switchb.org>
2024-06-07 08:57:53 -0700

    Add documentation for  “when to use this library”.
    Fixes <https://github.com/kpreid/rendiff/issues/8>.

--- rendiff/src/lib.rs
+++ rendiff/src/lib.rs
@@ -4,6 +4,34 @@
 //! of the same scene which were rendered using different algorithms, or different
 //! hardware, causing small “rounding errors” in either color or spatial position.
 //!
+//! To use it, call [`diff()`] on your images, and test the result against a [`Threshold`].
+// TODO: Add example code here.
+//!
+//! ## When to use this library
+//!
+//! `rendiff` is *not* a “perceptual” difference algorithm; it does not attempt to calculate
+//! how visible a difference is to the eye.
+//! Rather, it is intended to allow for various kinds of numerical error or arbitrary
+//! choice in rendering, which happen to also be perceptually insignificant:
+//!
+//! * Spatial: A line or curve being considered to fall on one side or another of a pixel center.
+//! * Color: A color value being rounded up or down.
+//!
+//! This algorithm will always ignore spatial errors which meet the following criteria:
+//!
+//! * the spatial offset is at most 1 pixel,
+//! * there are no 1-pixel-sized shapes that vanish entirely
+//!   (e.g. imagine a very narrow, pointy, non-axis-aligned triangle;
+//!   its sharp end turns into a series of disconnected dots), and
+//! * there isn't antialiasing which introduces miscellaneous intermediate shades.
+//!
+//! Therefore, `rendiff` is ideal for comparing non-antialiased renderings of “vector” graphics.
+//! In other situations, and for color rounding differences, you must tune the
+//! [`Threshold`] for allowed differences.
+//!
+//! `rendiff` is unsuitable for comparing images which have strong noise (e.g. halftone)
+//! or spatial displacements of more than one pixel.
+//!
 //! ## Principle of operation
 //!
 //! Suppose we are comparing two images, A and B.
@@ -17,7 +45,8 @@
 //! and a histogram (for pass/fail conditions).
 //!
 //! The effect of this strategy is that any feature in the image, such as the edge of a
-//! shape, can be displaced by up to the neighborhood size in any direction, thus
+//! shape, can be displaced by up to the neighborhood size (currently fixed to 1 pixel
+//! radius, i.e. a 3×3 neighborhood) in any direction, thus
 //! tolerating different choices of rounding into the pixel grid, as long as the color is
 //! the same.
 //!
